62e5b46770f295d1f9799524e24301810f5b6714
Riyas Moidu <moidu.riyas@hexagon.com>
2024-05-04 10:22:55 +0530

    add_function completed

--- code.py
+++ code.py
@@ -2,16 +2,20 @@ import argparse
 
 # Getting inpus from user
 parser = argparse.ArgumentParser(description="Simple Calculator")
-parser.add_argument("action", type=str, 
+parser.add_argument("operation", type=str, 
                     help="Choose 'add' for Addition, 'sub' for Subtraction, 'div' for Division, and 'mul' for Multiplication")
 parser.add_argument("x", type=float, help="Value of X")
 parser.add_argument("y", type=float, help="Value of Y")
 args = parser.parse_args()
 
 # function for addition -<TBD>
-
+def add(x=args.x, y=args.y):
+    return x+y
 # function for subtraction -<TBD>
 
 # function for division -<TBD>
 
 # function for multiplication -<TBD>
+
+result = eval(f"{args.operation}()")
+print(f"The action {args.operation} between {args.x} and {args.y} resulted in {result}")
